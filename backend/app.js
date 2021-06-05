c846e82b2a486f7e084a69478a2a5242ab60dff0
Jovo <jovo.simic@apeiron-edu.eu>
2021-06-05 19:23:12 +0200

    setup database

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 require("dotenv/config");
 
 const app = express();
 
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    writeConcern: {
+      j: true,
+    },
+  })
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log(err));
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
